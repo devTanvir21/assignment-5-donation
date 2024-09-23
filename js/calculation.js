b165f99c299badf772891e8827bc4d31374ed966
devTanvir21 <tanvirahmed21.ta@gmail.com>
2024-09-23 19:09:08 +0600

    modal completed

--- js/calculation.js
+++ js/calculation.js
@@ -1,5 +1,26 @@
 // redirecting to the blog page
 const btnBlog = document.getElementById('btn-blog');
-btnBlog.addEventListener('click', function(){
+btnBlog.addEventListener('click', function () {
     window.location.href = '/blog.html';
 })
+
+// // for calculating donation amount
+// let amountInHand = document.getElementById('amount-in-hand').innerText;
+
+// let amountNoakhali = document.getElementById('amount-noakhali').innerText;
+// let amountFeni = document.getElementById('amount-feni').innerText;
+// let amountQuota = document.getElementById('amount-quota').innerText;
+
+// let inputNoakhali = document.getElementById('input-noakhali').value;
+// let inputFeni = document.getElementById('input-feni').value;
+// let inputQuota = document.getElementById('input-quota').value;
+
+// let btnFeni = document.getElementById('btn-feni');
+// let btnQuota = document.getElementById('btn-quota');
+
+// const btnNoakhali = document.getElementById('btn-noakhali');
+
+// btnNoakhali.addEventListener('click', function () {
+//     console.log('noakhali');
+
+// });
