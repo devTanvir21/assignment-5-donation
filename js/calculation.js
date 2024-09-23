0697ee22a08ba854cbe863c56d483bd4bd2bf881
devTanvir21 <tanvirahmed21.ta@gmail.com>
2024-09-24 00:17:19 +0600

    toggle between donation and history

--- js/calculation.js
+++ js/calculation.js
@@ -3,49 +3,64 @@ const btnBlog = document.getElementById('btn-blog');
 btnBlog.addEventListener('click', function () {
     window.location.href = './blog.html';
 })
-// redirecting to the index/main page
+
+
 const btnDonation = document.getElementById('btn-donation');
-btnDonation.addEventListener('click', function () {
-    window.location.href = './index.html';
-})
-// redirecting to the history page
 const btnHistory = document.getElementById('btn-history');
-// btnHistory.addEventListener('click', function () {
-//     window.location.href = './history.html';
-// })
+
+const donationSection = document.getElementById('donation-section');
+const historySection = document.getElementById('history-section');
 
 
-// for btn active/inactive
+// for btn active/inactive && class hidden
 btnDonation.addEventListener('click', function () {
-    btnDonation.classList.replace('bg-[#B4F461]', 'bg-white');
-    btnHistory.classList.replace('bg-white', 'bg-[#B4F461]');
+    if (btnDonation.classList.contains('bg-white')) {
+        btnDonation.classList.replace('bg-white', 'bg-[#B4F461]');
+        btnHistory.classList.replace('bg-[#B4F461]', 'bg-white');
+    } else {
+        btnDonation.classList.replace('bg-[#B4F461]', 'bg-white');
+        btnHistory.classList.replace('bg-white', 'bg-[#B4F461]');
+    }
+
+    donationSection.classList.remove('hidden');
+    historySection.classList.add('hidden');
+
 });
 
 btnHistory.addEventListener('click', function () {
-    btnHistory.classList.replace('bg-white', 'bg-[#B4F461]');
-    btnDonation.classList.replace('bg-[#B4F461]', 'bg-white');
+    if (btnHistory.classList.contains('bg-white')) {
+        btnHistory.classList.replace('bg-white', 'bg-[#B4F461]');
+        btnDonation.classList.replace('bg-[#B4F461]', 'bg-white');
+    } else {
+        btnHistory.classList.replace('bg-[#B4F461]', 'bg-white');
+        btnDonation.classList.replace('bg-white', 'bg-[#B4F461]');
+    }
+
+    donationSection.classList.add('hidden');
+    historySection.classList.remove('hidden');
 });
 
 
 // For calculating donation amount
-const amountInHand = document.getElementById('amount-in-hand').innrText;
-const amountInHandNumber = parseFloat(amountInHand);
+const amountInHand = document.getElementById('amount-in-hand');
+const amountInHandNumber = parseFloat(amountInHand.innerText);
 
 function handleDonation(amountId, inputId) {
-    const amount = document.getElementById(amountId).innerText;
-    const amountInNumber = parseFloat(amount);
+    const amount = document.getElementById(amountId);
+    const amountInNumber = parseFloat(amount.innerText);
 
     const input = document.getElementById(inputId);
     const inputInNumber = parseFloat(input.value);
-    if(inputInNumber !== 'number' || inputInNumber <= 0){
+
+    if(inputInNumber <= 0){
         return alert('Invalid Input! Please try a valid amount!!');
     }
 
     const updatedAmount = amountInNumber + inputInNumber;
-    amount= updatedAmount;
+    amount.innerText = updatedAmount;
 
     const newAmountInHand = parseFloat(amountInHand.innerText) - inputInNumber;
-    amountInHand = newAmountInHand;
+    amountInHand.innerText = newAmountInHand;
 
     input.value = '';
 
