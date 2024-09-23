e7c1de851b27e983fbf5b93c05b46edd5146c9c2
devTanvir21 <tanvirahmed21.ta@gmail.com>
2024-09-23 17:44:22 +0600

    redirecting to the blog page

--- js/calculation.js
+++ js/calculation.js
@@ -1,1 +1,5 @@
-console.log('got clicked');
+// redirecting to the blog page
+const btnBlog = document.getElementById('btn-blog');
+btnBlog.addEventListener('click', function(){
+    window.location.href = '/blog.html';
+})
